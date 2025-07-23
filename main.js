85f71daea6f333177a9fa85c9ee74d994b968f06
guilhermecolares <guilhermegraf17@gmail.com>
2025-07-23 12:08:21 -0300

    MELHORIA DE CODIGO + MELHORANDO A RESPOSIVIDADE

--- main.js
+++ main.js
@@ -7,35 +7,44 @@ const swiper = new Swiper('.mySwiper', {
         el: '.swiper-pagination',
         clickable: true,
     },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    // },
     breakpoints: {
-        768: {
+        1300: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            centeredSlides: false
+        },
+        1024: {
             slidesPerView: 2,
             spaceBetween: 20,
         },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 30,
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 20,
         },
         320: {
             slidesPerView: 1,
             spaceBetween: 10,
         }
     }
-});
+})
 
-const prevButton = document.querySelector('#prevButton');
-const nextButton = document.querySelector('#nextButton');
+swiper.on('breakpoint', function (swiper, breakpointParams) {
+    console.log('Breakpoint ativado:', breakpointParams)
+})
+
+const prevButton = document.querySelector('#prevButton')
+const nextButton = document.querySelector('#nextButton')
 
 if (prevButton && nextButton) {
     prevButton.addEventListener('click', () => {
-        swiper.slidePrev();
-    });
+        swiper.slidePrev()
+    })
 
     nextButton.addEventListener('click', () => {
-        swiper.slideNext();
-    });
+        swiper.slideNext()
+    })
 }
